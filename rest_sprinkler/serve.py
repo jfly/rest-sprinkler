8d4ed1831e03298efec4d8a6c47642c2ce12bbc8
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-09-02 10:06:07 -0700

    Default to a port that does not conflict with OpenSprinkler

--- rest_sprinkler/serve.py
+++ rest_sprinkler/serve.py
@@ -96,5 +96,5 @@ class HttpServer(socketserver.TCPServer):
 
 
 if __name__ == "__main__":
-    server = HttpServer("127.0.0.1", 8080, Handler)
+    server = HttpServer("127.0.0.1", 8081, Handler)
     server.serve_forever()
